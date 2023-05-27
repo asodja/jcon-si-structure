e37ac8465b4afa3e6a85acf874e21f22ffcd5f70
Anže Sodja <asodja@gradle.com>
2023-05-27 15:05:21 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "jcon-si-structure"
+include(
+    "core",
+    "library",
+    "web-app"
+)
+rootProject.children.forEach {
+    it.projectDir = file("projects/${it.name}")
+}
